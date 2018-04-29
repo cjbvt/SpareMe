c6ab69cfdbabbda36886252423ae5eff2f8c8931
cjbvt <cjroboto@protonmail.ch>
2018-04-28 22:28:00 -0400

    Preprocess all input to fastText classifier
    Closes #117

--- api/flask/ftclassifier.py
+++ api/flask/ftclassifier.py
@@ -1,6 +1,7 @@
 import fastText
 import os
 import tempfile
+from subprocess import Popen, PIPE, STDOUT
 
 import dal
 
@@ -9,12 +10,29 @@ import dal
 
 label_prefix = "__label__"
 
+def preprocess(datum):
+    # strip exclamation points and newlines
+    stripped_datum = datum.replace('!','').replace('\r','').replace('\n','')
+
+    # convert to lowercase
+    lowercased_datum = stripped_datum.lower()
+
+    # tokenize the datum
+    p = Popen(["perl", "tokenizer.perl", "-q"], stdout=PIPE, stdin=PIPE)
+    tokenized_datum = p.communicate(input=lowercased_datum.encode())[0].decode()
+
+    # strip any newlines added by tokenizer or Popen
+    return tokenized_datum.replace('\r','').replace('\n','')
+
 def fit(session, uid, path):
     labeled_text = dal.get_text_labeled_text(session, uid)
 
     # not sure if this is the right prereq for fasttext
     if len(labeled_text['targets']) < 2:
         return
+
+    # preprocess training data one line at a time in order to limit pipe buffer issues
+    preprocessed_data = [preprocess(datum) for datum in labeled_text['data']]
 
     # create a new temporary training data file
     fd, train_path = tempfile.mkstemp()
@@ -24,7 +42,7 @@ def fit(session, uid, path):
 
     # fill the temporary training data file
     with open(train_path, 'w') as f:
-        for (target, datum) in zip(labeled_text['targets'], labeled_text['data']):
+        for (target, datum) in zip(labeled_text['targets'], preprocessed_data):
             f.write(label_prefix + target + " " + datum + "\n")
 
     # train the fasttext model
@@ -40,11 +58,14 @@ def fit(session, uid, path):
     model.save_model(path)
 
 def predict(uid, path, unlabeled_text):
+    # preprocess training data one line at a time in order to limit pipe buffer issues
+    preprocessed_data = [preprocess(datum) for datum in unlabeled_text]
+
     # deserialize the model in from the temporary model file
     model = fastText.load_model(path)
 
     # use the model to predict the most likely label for each of the given strings
-    all_labels, _ = model.predict(unlabeled_text)
+    all_labels, _ = model.predict(preprocessed_data)
 
     # strip off the label prefix and return only the most likely label for each unlabeled text string
     return [labels[0].replace(label_prefix,'') for labels in all_labels]
